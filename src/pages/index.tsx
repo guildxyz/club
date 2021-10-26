ee5bf1f14b32224239e53e0d52efc465bf52e686
BrickheadJohnny <johnny.zgen@gmail.com>
2021-10-26 17:16:02 +0200

    Airdrop page cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,55 +2,30 @@ import { Button, Heading, Text, VStack } from "@chakra-ui/react"
 import Card from "components/common/Card"
 import JoinCommunity from "components/common/JoinCommunity"
 import Layout from "components/common/Layout"
-import { GetStaticProps } from "next"
-import { Data } from "temporaryData/types"
 
-type Props = {
-  data: Data[]
-}
+const Page = (): JSX.Element => (
+  <Layout title="Airdrop">
+    <Card p={8} fontFamily="display">
+      <VStack spacing={8} fontWeight="semibold" textAlign="center">
+        <Heading as="h1" fontFamily="display" fontSize={{ base: "4xl", md: "5xl" }}>
+          Seed Club Airdrop
+        </Heading>
 
-const Page = ({ data: dataInitial }: Props): JSX.Element => {
-  // const { data } = useSWR("data", fetchData, {
-  //   fallbackData: dataInitial,
-  // })
+        <Text fontSize="4xl">22:40:58</Text>
 
-  return (
-    <Layout title="Airdrop">
-      <Card p={8} fontFamily="display">
-        <VStack spacing={8} fontWeight="semibold" textAlign="center">
-          <Heading
-            as="h1"
-            fontFamily="display"
-            fontSize={{ base: "4xl", md: "5xl" }}
-          >
-            Seed Club Airdrop
-          </Heading>
-
-          <Text fontSize="4xl">22:40:58</Text>
-
-          <VStack spacing={1} fontSize="xl" pb={4}>
-            <Text>You are on the whitelist</Text>
-            <Text>2 $CLUB waiting to be claimed</Text>
-          </VStack>
-
-          <Button px={8} letterSpacing="wide" colorScheme="seedclub">
-            Claim
-          </Button>
-
-          <JoinCommunity />
+        <VStack spacing={1} fontSize="xl" pb={4}>
+          <Text>You are on the whitelist</Text>
+          <Text>2 $CLUB waiting to be claimed</Text>
         </VStack>
-      </Card>
-    </Layout>
-  )
-}
 
-export const getStaticProps: GetStaticProps = async () => {
-  // const data = await fetchData()
+        <Button px={8} letterSpacing="wide" colorScheme="seedclub">
+          Claim
+        </Button>
 
-  return {
-    props: { data: {} },
-    revalidate: 10,
-  }
-}
+        <JoinCommunity />
+      </VStack>
+    </Card>
+  </Layout>
+)
 
 export default Page
