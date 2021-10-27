775809ba4bfecb8dacc4933af53e12b23d3e303a
BrickheadJohnny <johnny.zgen@gmail.com>
2021-10-27 17:12:55 +0200

    MerkleDistributor constant

---
+++ src/constants/MerkleDistributor.ts
@@ -0,0 +1,49 @@
+const data = {
+  merkleRoot: "0x7ca720aa612c950647ff310045ad14f17405ebc073a846fd66de5ade741dd894",
+  tokenTotal: "0x2086ac351052600000",
+  claims: {
+    "0x2893b7e6E8a5aF81d262024a550a3159b1F65217": {
+      index: 0,
+      amount: "0x056BC75E2D63100000",
+      proof: [
+        "0x2bc9535957d08aa19d50fc5ee958443c05fb83713bd5b01757ee6b8ebe3b5615",
+        "0x7a37f77ccda75aa4faf04c458372291bd08f87080bf1d022e587d0ccd81ceede",
+        "0xe1545076b300b3da1be1bd468eb174ba07fab2df0c664f7e296f6ba348b317d7",
+      ],
+    },
+    "0x627522F78227DB80c16774ab803541eB523eDDdc": {
+      index: 1,
+      amount: "0x056BC75E2D63100000",
+      proof: [
+        "0xd9be73e81ad2ca31642ca80c220ad9e4a0de7d006615a37879e31550e038b867",
+        "0x79cadfb389a9b2395ce5f8d745d2c70ef65a79b1fc48ef6e4846e4e97a9e30d7",
+        "0xe1545076b300b3da1be1bd468eb174ba07fab2df0c664f7e296f6ba348b317d7",
+      ],
+    },
+    "0x9d5769b6c8150Dc2E8c6A7c1F2D07C5aAdeD6327": {
+      index: 2,
+      amount: "0x056BC75E2D63100000",
+      proof: ["0xf913f03ad81fe4e35a68354564d52b96aaca5011468656788f59e3493a4e3850"],
+    },
+    "0xA861C07fEc05525F2DB7408E8C3319B8D8929B78": {
+      index: 3,
+      amount: "0x0AD78EBC5AC6200000",
+      proof: [
+        "0x1dbd9dfd2c29e72eb3793d4d299c3530fcc7d01b2c2d0f5672152161d4673b8e",
+        "0x7a37f77ccda75aa4faf04c458372291bd08f87080bf1d022e587d0ccd81ceede",
+        "0xe1545076b300b3da1be1bd468eb174ba07fab2df0c664f7e296f6ba348b317d7",
+      ],
+    },
+    "0xe991DD89298C9514E37fB9CF63FB89B6653C5A62": {
+      index: 4,
+      amount: "0x056BC75E2D63100000",
+      proof: [
+        "0x659fe8dc847ae75c27286ca8ba50fec65334012015863fdec341306cd1d89060",
+        "0x79cadfb389a9b2395ce5f8d745d2c70ef65a79b1fc48ef6e4846e4e97a9e30d7",
+        "0xe1545076b300b3da1be1bd468eb174ba07fab2df0c664f7e296f6ba348b317d7",
+      ],
+    },
+  },
+}
+
+export default data
