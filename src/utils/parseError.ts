098923036f09635342b397619e9fbd44e3d22c72
BrickheadJohnny <johnny.zgen@gmail.com>
2021-11-04 10:00:43 +0100

    Improved parseError function

--- src/utils/parseError.ts
+++ src/utils/parseError.ts
@@ -1,8 +1,23 @@
 import { Logger } from "@ethersproject/logger"
 
 const parseError = (err: any): string => {
-  if (Object.values(Logger.errors).includes(err.code))
+  if (Object.values(Logger.errors).includes(err.code)) {
+    const firstSplit = err.toString().split("(")
+
+    if (firstSplit[1]) {
+      // Return a more specific error message
+      const errorObj = firstSplit[1].split(", ")[0].replace("error=", "")
+      try {
+        const error = JSON.parse(errorObj)
+        return error.message
+      } catch (_) {
+        return "An unknown error occurred!"
+      }
+    }
+
+    // Return the "general" error message
     return err.toString().split("(")[0]
+  }
 
   return err?.message
 }
