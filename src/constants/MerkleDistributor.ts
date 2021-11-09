1512ad93e817caea817386439c507d3816e26d1d
BrickheadJohnny <johnny.zgen@gmail.com>
2021-11-09 20:04:27 +0100

    New merkle distributor & incentive

--- src/constants/MerkleDistributor.ts
+++ src/constants/MerkleDistributor.ts
@@ -1,47 +1,47 @@
 const data = {
-  merkleRoot: "0x729e8965937d85fa877489f2f1a93953807f3941dcd46c2bedf88ce417c143c9",
-  tokenTotal: "0x4563918244f40000",
+  merkleRoot: "0xd67abe7a9d22138d5b70534b35000da05d8776ed78e28873108525ad9fe49c13",
+  tokenTotal: "0x1b1ae4d6e2ef500000",
   claims: {
-    "0x2893b7e6E8a5aF81d262024a550a3159b1F65217": {
+    "0x14DDFE8EA7FFc338015627D160ccAf99e8F16Dd3": {
       index: 0,
-      amount: "0x0DE0B6B3A7640000",
+      amount: "0x056BC75E2D63100000",
       proof: [
-        "0x8facf0224c17f0d7af947f4d013e0351713052964f16d56bebaa193480d79592",
-        "0xe153a417a5f866a1aa28b3eaf908ce53621230d9b6f8b0fdd6faea2a413d302d",
-        "0xf18506aebc1d6d14ab232164b0a242cc791b24a2ba0c9d5be291fb98303ac4d7",
+        "0x88bc69c158e9a75218c436b32070eb86b48c5e65ae3e4a517b08dc84a09ecad8",
+        "0xbae405801deb3d3ca5c432ea59aaef3680f706a34f25fec02d5d3de1bac5f7b4",
+        "0xe6a4d3fcf94bd2d2770e62d15dac9a1271684c5954338e6d1ab6330702e791fe",
       ],
+    },
+    "0x2893b7e6E8a5aF81d262024a550a3159b1F65217": {
+      index: 1,
+      amount: "0x056BC75E2D63100000",
+      proof: ["0x29e40f26a7b7b2915ffbe3ecc6167cb2b96818768176346fb805e2ab98ce6cf9"],
     },
     "0x627522F78227DB80c16774ab803541eB523eDDdc": {
-      index: 1,
-      amount: "0x0DE0B6B3A7640000",
-      proof: [
-        "0xa32e8b62714c70bc2b5ec0dd848cc7a199c259c240003cb7fc389ff437203809",
-        "0xe153a417a5f866a1aa28b3eaf908ce53621230d9b6f8b0fdd6faea2a413d302d",
-        "0xf18506aebc1d6d14ab232164b0a242cc791b24a2ba0c9d5be291fb98303ac4d7",
-      ],
-    },
-    "0x9d5769b6c8150Dc2E8c6A7c1F2D07C5aAdeD6327": {
       index: 2,
-      amount: "0x0DE0B6B3A7640000",
+      amount: "0x056BC75E2D63100000",
       proof: [
-        "0x6bc7ea93718af8b940fdea7c9bed31ab5cb8601a0947e4153d4fccd1e37c3996",
-        "0x7fda5510d5b0dddb1245c319e3ac64f68d0dc29149e686a6f272d005583b1cd2",
-        "0xf18506aebc1d6d14ab232164b0a242cc791b24a2ba0c9d5be291fb98303ac4d7",
+        "0xa32b8d6d0677ebd9dcb03041cfc3efb991b5125dc087a9954ce1430d3a52319a",
+        "0xbae405801deb3d3ca5c432ea59aaef3680f706a34f25fec02d5d3de1bac5f7b4",
+        "0xe6a4d3fcf94bd2d2770e62d15dac9a1271684c5954338e6d1ab6330702e791fe",
       ],
     },
     "0xA861C07fEc05525F2DB7408E8C3319B8D8929B78": {
       index: 3,
-      amount: "0x0DE0B6B3A7640000",
+      amount: "0x056BC75E2D63100000",
       proof: [
-        "0x4fe4cadcb87f874759b98b46a94ebfbfa6a1e985a3f6fd5741f61bf73be61159",
-        "0x7fda5510d5b0dddb1245c319e3ac64f68d0dc29149e686a6f272d005583b1cd2",
-        "0xf18506aebc1d6d14ab232164b0a242cc791b24a2ba0c9d5be291fb98303ac4d7",
+        "0xd9be73e81ad2ca31642ca80c220ad9e4a0de7d006615a37879e31550e038b867",
+        "0xf1dae524932c893029d078d1a6e7386b01859ca893603811a7baf74f83509609",
+        "0xe6a4d3fcf94bd2d2770e62d15dac9a1271684c5954338e6d1ab6330702e791fe",
       ],
     },
     "0xe991DD89298C9514E37fB9CF63FB89B6653C5A62": {
       index: 4,
-      amount: "0x0DE0B6B3A7640000",
-      proof: ["0x7705a096a0eb12400f2b83525e5f8715664c3e296ae610b8a688dbaffc19a039"],
+      amount: "0x056BC75E2D63100000",
+      proof: [
+        "0xa4af21c880fbff56a6f52ef2b949a7b16a21146f05ea1bb2acf3ec9f90963148",
+        "0xf1dae524932c893029d078d1a6e7386b01859ca893603811a7baf74f83509609",
+        "0xe6a4d3fcf94bd2d2770e62d15dac9a1271684c5954338e6d1ab6330702e791fe",
+      ],
     },
   },
 }
