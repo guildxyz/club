ca73cc15cd4a89595a2b18b9890044320beb69fc
BrickheadJohnny <johnny.zgen@gmail.com>
2021-12-30 14:50:20 +0100

    Updated RPC URL

--- src/connectors.ts
+++ src/connectors.ts
@@ -3,31 +3,17 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
 
 enum Chains {
   ETHEREUM = 1,
-  ROPSTEN = 3,
   GOERLI = 5,
-  BSC = 56,
-  BSCTEST = 97,
-  POLYGON = 137,
 }
 
 const RPC = {
-  POLYGON: {
-    chainId: "0x89",
-    chainName: "Matic",
-    nativeCurrency: {
-      name: "Polygon",
-      symbol: "MATIC",
-      decimals: 18,
-    },
-    rpcUrls: ["https://polygon-rpc.com/"],
-    blockExplorerUrls: ["https://polygonscan.com/"],
-    iconUrls: ["/networkLogos/polygon.svg"],
-  },
   ETHEREUM: {
     chainName: "Ethereum",
     blockExplorerUrls: ["https://etherscan.io/"],
     iconUrls: ["/networkLogos/ethereum.svg"],
-    rpcUrls: ["https://main-light.eth.linkpool.io/"],
+    rpcUrls: [
+      "https://eth-mainnet.alchemyapi.io/v2/yiNnGigifqu96_5ys970MfkDBl2z2Or9",
+    ],
   },
   GOERLI: {
     chainName: "Goerli",
@@ -35,21 +21,9 @@ const RPC = {
     iconUrls: ["/networkLogos/ethereum.svg"],
     rpcUrls: ["https://goerli-light.eth.linkpool.io/"],
   },
-  BSC: {
-    chainId: "0x38",
-    chainName: "BSC",
-    nativeCurrency: {
-      name: "Binance Coin",
-      symbol: "BNB",
-      decimals: 18,
-    },
-    rpcUrls: ["https://bsc-dataseed.binance.org/"],
-    blockExplorerUrls: ["https://bscscan.com/"],
-    iconUrls: ["/networkLogos/bsc.svg"],
-  },
 }
 
-const supportedChains = [, /* "POLYGON", "BSC", "GOERLI", */ "ETHEREUM"]
+const supportedChains = [, /* "GOERLI", */ "ETHEREUM"]
 const supportedChainIds = supportedChains.map((_) => Chains[_])
 
 const injected = new InjectedConnector({ supportedChainIds })
